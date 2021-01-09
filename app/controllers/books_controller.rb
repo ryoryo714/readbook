b99eec5bb7859613f389fcbd4c073b0e35877eb9
ryoryo714 <ilovesoccerhr94@gmail.com>
2021-01-09 12:59:56 +0900

    Revert "ユーザーツイート紐付け"
    This reverts commit ffe29d1ff9ecc2d91dec2d7725a947daa620719b.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :image, :read_book).merge(user_id: current_user.id)
+    params.require(:book).permit(:title, :image, :read_book)
   end
 
   def move_to_index
